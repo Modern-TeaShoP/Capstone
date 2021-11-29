12bb6f182e12821717a42e4f01844908167270a3
Joseph Elias <jrelias@outlook.com>
2021-11-29 16:25:04 -0500

    socket working, extra player still loading

--- server/socket/index.js
+++ server/socket/index.js
@@ -17,14 +17,44 @@ module.exports = (io) => {
 
   socket.on("joinRoom", (roomKey) => {
       socket.join(roomKey);
-      // const roomInfo = gameRooms[roomKey];
-      // console.log("roomInfo", roomInfo);
-      // roomInfo.players[socket.id] = {
-      //   rotation: 0,
-      //   x: 400,
-      //   y: 300,
-      //   playerId: socket.id,
+      const roomInfo = gameRooms[roomKey];
+      console.log("roomInfo", roomInfo);
+      roomInfo.players[socket.id] = {
+        rotation: 0,
+        x: 400,
+        y: 300,
+        playerId: socket.id,
+      };
+
+       // update number of players
+      roomInfo.numPlayers = Object.keys(roomInfo.players).length;
+
+      // set initial state
+      socket.emit("setState", roomInfo);
+
+      // send the players object to the new player
+      socket.emit("currentPlayers", {
+        players: roomInfo.players,
+        numPlayers: roomInfo.numPlayers,
       });
+
+       // update all other players of the new player
+      socket.to(roomKey).emit("newPlayer", {
+        playerInfo: roomInfo.players[socket.id],
+        numPlayers: roomInfo.numPlayers,
+      });
+    });
+
+      // when a player moves, update the player data
+    socket.on("playerMovement", function (data) {
+      const { x, y, roomKey } = data;
+      gameRooms[roomKey].players[socket.id].x = x;
+      gameRooms[roomKey].players[socket.id].y = y;
+      // emit a message to all players about the player that moved
+      socket
+        .to(roomKey)
+        .emit("playerMoved", gameRooms[roomKey].players[socket.id]);
+    });
 
   socket.on("isKeyValid", function (input) {
       Object.keys(gameRooms).includes(input)
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -3,19 +3,133 @@ import "phaser";
 export default class MainScene extends Phaser.Scene {
     constructor() {
         super("MainScene");
+        this.state = {};
     }
 
     create() {
+        const scene = this;
         // << LOAD BACKGROUND AND FOREGROUND SCENES IN PARALLEL HERE >>
         this.scene.launch("BgScene");
         this.scene.launch("FgScene");
+
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         //CREATE SOCKET
         this.socket = io();
 
         //LAUNCH WAITING ROOM
-        this.scene.launch("WaitingRoom", {socket: this.scene.socket})
+        scene.scene.launch("WaitingRoom", {socket: scene.socket})
+
+        // CREATE OTHER PLAYERS GROUP
+        this.otherPlayers = this.physics.add.group();
+
+        // JOINED ROOM - SET STATE
+        this.socket.on("setState", function (state) {
+        const { roomKey, players, numPlayers } = state;
+        scene.physics.resume();
+
+        // STATE
+        scene.state.roomKey = roomKey;
+        scene.state.players = players;
+        scene.state.numPlayers = numPlayers;
+        });
+
+            // PLAYERS
+    this.socket.on("currentPlayers", function (arg) {
+      const { players, numPlayers } = arg;
+      scene.state.numPlayers = numPlayers;
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === scene.socket.id) {
+          scene.addPlayer(scene, players[id]);
+        } else {
+          scene.addOtherPlayers(scene, players[id]);
+        }
+      });
+    });
+
+    this.socket.on("newPlayer", function (arg) {
+      const { playerInfo, numPlayers } = arg;
+      scene.addOtherPlayers(scene, playerInfo);
+      scene.state.numPlayers = numPlayers;
+    });
+
+    this.socket.on("playerMoved", function (playerInfo) {
+      scene.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerInfo.playerId === otherPlayer.playerId) {
+          const oldX = otherPlayer.x;
+          const oldY = otherPlayer.y;
+          otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+        }
+      });
+    });
     }
 
-    update(){}
+    update(){
+        const scene = this;
+    //MOVEMENT
+    if (this.octo) {
+      const speed = 225;
+      const prevVelocity = this.octo.body.velocity.clone();
+      // Stop any previous movement from the last frame
+      this.octo.body.setVelocity(0);
+      // Horizontal movement
+      if (this.cursors.left.isDown) {
+        this.octo.body.setVelocityX(-speed);
+      } else if (this.cursors.right.isDown) {
+        this.octo.body.setVelocityX(speed);
+      }
+      // Vertical movement
+      if (this.cursors.up.isDown) {
+        this.octo.body.setVelocityY(-speed);
+      } else if (this.cursors.down.isDown) {
+        this.octo.body.setVelocityY(speed);
+      }
+      // Normalize and scale the velocity so that octo can't move faster along a diagonal
+      this.octo.body.velocity.normalize().scale(speed);
+
+            // emit player movement
+      var x = this.octo.x;
+      var y = this.octo.y;
+      if (
+        this.octo.oldPosition &&
+        (x !== this.octo.oldPosition.x ||
+          y !== this.octo.oldPosition.y)
+      ) {
+        this.moving = true;
+        this.socket.emit("playerMovement", {
+          x: this.octo.x,
+          y: this.octo.y,
+          roomKey: scene.state.roomKey,
+        });
+      }
+      // save old position data
+      this.octo.oldPosition = {
+        x: this.octo.x,
+        y: this.octo.y,
+        rotation: this.octo.rotation,
+      };
+    }
+
+    }
+
+
+
+    addPlayer(scene, playerInfo) {
+    scene.joined = true;
+    scene.octo = scene.physics.add
+      .sprite(playerInfo.x, playerInfo.y, "octo")
+      .setOrigin(0.5, 0.5)
+      .setSize(30, 40)
+      .setOffset(0, 24);
+  }
+
+    addOtherPlayers(scene, playerInfo) {
+    const otherPlayer = scene.add.sprite(
+      playerInfo.x + 40,
+      playerInfo.y + 40,
+      "octo"
+    );
+    otherPlayer.playerId = playerInfo.playerId;
+    scene.otherPlayers.add(otherPlayer);
+  }
 }
