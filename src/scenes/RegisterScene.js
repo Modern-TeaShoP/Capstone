674e423484f6c701f83ad72688333caa74965a5b
Rusty Porter <rusty.porter@gmail.com>
2021-12-08 07:40:25 -0700

    Registration feels cyber

--- src/scenes/RegisterScene.js
+++ src/scenes/RegisterScene.js
@@ -1,9 +1,9 @@
-import Phaser from 'phaser';
-import axios from 'axios';
+import Phaser from "phaser";
+import axios from "axios";
 
 export default class RegisterScene extends Phaser.Scene {
   constructor() {
-    super('RegisterScene');
+    super("RegisterScene");
     this.state = {};
     this.hasBeenSet = false;
   }
@@ -13,7 +13,8 @@ export default class RegisterScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.html('registerform', 'assets/text/registerform.html');
+    this.load.html("registerform", "assets/text/registerform.html");
+    this.load.image("octopus", "assets/sprites/octopus.png");
   }
 
   create() {
@@ -23,50 +24,55 @@ export default class RegisterScene extends Phaser.Scene {
     scene.boxes = scene.add.graphics();
 
     // for popup window
-    scene.popUp.lineStyle(1, 0xffffff);
+    scene.popUp.lineStyle(1, 0x60ceff);
     scene.popUp.fillStyle(0xffffff, 0.5);
 
     // for boxes
-    scene.boxes.lineStyle(1, 0xffffff);
+    scene.boxes.lineStyle(1, 0x60ceff);
     scene.boxes.fillStyle(0xa9a9a9, 1);
 
     // popup window
-    scene.popUp.strokeRect(25, 25, 750, 500);
-    scene.popUp.fillRect(25, 25, 750, 500);
+    const screenCenterX = this.cameras.main.width / 4;
+    const screenCenterY = this.cameras.main.height / 4;
+    scene.popUp.strokeRect(screenCenterX, screenCenterY, 750, 500);
+    scene.popUp.fillRect(screenCenterX, screenCenterY, 750, 500);
+    scene.logo = scene.add.image(screenCenterX, screenCenterY, "octopus");
 
     //title
-    scene.title = scene.add.text(100, 75, 'Register New User', {
-      fill: '#add8e6',
-      fontSize: '66px',
-      fontStyle: 'bold',
-    });
+    scene.title = scene.add
+      .text(screenCenterX + 35, screenCenterY + 70, "Register New User", {
+        fill: "#ff30e9",
+        fontSize: "66px",
+        fontStyle: "bold",
+      })
+      .setShadow(0, 0, "#000000", 10, false, true);
 
     //right popup
-    scene.boxes.strokeRect(250, 200, 275, 100);
-    scene.boxes.fillRect(250, 200, 275, 100);
+    scene.boxes.strokeRect(screenCenterX + 225, screenCenterY + 200, 275, 100);
+    scene.boxes.fillRect(screenCenterX + 225, screenCenterY + 200, 275, 100);
     scene.inputElement = scene.add
-      .dom(400, 250)
-      .createFromCache('registerform');
-    scene.inputElement.addListener('click');
-    scene.inputElement.on('click', async function (event) {
-      if (event.target.name === 'submitRegister') {
-        const email = scene.inputElement.getChildByName('email').value;
+      .dom(screenCenterX + 360, screenCenterY + 250)
+      .createFromCache("registerform");
+    scene.inputElement.addListener("click");
+    scene.inputElement.on("click", async function (event) {
+      if (event.target.name === "submitRegister") {
+        const email = scene.inputElement.getChildByName("email").value;
 
-        const password = scene.inputElement.getChildByName('password').value;
+        const password = scene.inputElement.getChildByName("password").value;
 
-        const username = scene.inputElement.getChildByName('username').value;
+        const username = scene.inputElement.getChildByName("username").value;
 
         console.log(email, password);
 
-        const createdUser = await axios.post('http://localhost:8080/register', {
+        const createdUser = await axios.post("http://localhost:8080/register", {
           email,
           password,
           username,
         });
 
         if (createdUser !== null) {
-          scene.scene.stop('RegisterScene');
-          scene.scene.launch('LoginScene', { socket: scene.socket });
+          scene.scene.stop("RegisterScene");
+          scene.scene.launch("LoginScene", { socket: scene.socket });
           // scene.scene.start('WaitingRoom');
         }
 
