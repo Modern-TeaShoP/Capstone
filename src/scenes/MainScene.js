6d21d561b1f556f0b3158b6966b28081c3377bf3
Scott M Getz <scottgetz35@gmail.com>
2021-12-11 00:29:21 -0500

    Story Scene added with button functionality of both play and controls

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -2,22 +2,22 @@ import "phaser";
 import OctoGuy from "../entity/OctoGuy";
 
 export default class MainScene extends Phaser.Scene {
-  constructor() {
-    super("MainScene");
-    this.state = {};
-  }
+    constructor() {
+        super("MainScene");
+        this.state = {};
+    }
 
-  preload() {}
+    preload() {}
 
-  create() {
-    const scene = this;
+    create() {
+        const scene = this;
 
-    //CREATE SOCKET
-    this.socket = io();
+        //CREATE SOCKET
+        this.socket = io();
 
-    //LAUNCH LOGIN PAGE
-    scene.scene.launch("LoginScene", { socket: scene.socket });
-  }
+        //LAUNCH LOGIN PAGE
+        scene.scene.launch("StoryScene", { socket: scene.socket });
+    }
 
-  update() {}
+    update() {}
 }
