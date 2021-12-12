995c165b9b97f6f6ae64ec551c13cd9ef8e75ba1
Joseph Elias <jrelias@outlook.com>
2021-12-11 19:26:16 -0500

    modified db to connect to heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,26 @@
-const Sequelize = require("sequelize");
-const pkg = require("../../package.json");
+const Sequelize = require('sequelize');
+// const pkg = require("../../package.json");
 
-const databaseName = pkg.name;
+// const databaseName = pkg.name;
 
-const db = new Sequelize(
-    process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`
-);
+// const db = new Sequelize(
+//     process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`
+// );
+
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 module.exports = db;
