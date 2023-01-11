36b366123c090ea6463c6afc40825e32cd1c29ed
Joseph Elias <jrelias@outlook.com>
2023-01-11 17:50:32 -0500

    installed webpack cli

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,33 +1,33 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const User = db.define("user", {
-    username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        },
+const User = db.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-        },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
     },
-    email: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-            isEmail: {
-                msg: "Must be a valid Email!",
-            },
-        },
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: {
+        msg: 'Must be a valid Email!',
+      },
     },
+  },
 });
 
 module.exports = User;
